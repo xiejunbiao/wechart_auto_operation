fc3ec73920b5ec41225ebe2e60471dd3ba3ca0b6
xiejunbiao <996790454@qq.com>
2020-08-15 21:23:55 +0800

    [FIX]增减配置文件解析工具

--- util/get_conf.py
+++ util/get_conf.py
@@ -1,6 +1,24 @@
 import sys
-
-args = sys.argv[1:]
-print(args)
+import os
 import configparser
+args = sys.argv[1:]
 
+file_name = os.path.join(args[1], 'config.ini')
+
+
+def get_config():
+    config = configparser.ConfigParser()
+    config.read(file_name)
+    section = config.sections()
+    conf_ = {}
+    for sec in section:
+        conf_[sec] = {}
+        for key, values in  config.items(sec):
+            conf_[sec][key] = values
+
+    return conf_
+
+
+conf = get_config()
+
+# print(conf['toutiao'].keys())
